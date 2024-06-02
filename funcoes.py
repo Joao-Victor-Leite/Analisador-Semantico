932d5504184f41404734550d6dedd13711e059de
João Victor Leite <jvldsa10@hotmail.com>
2024-06-02 19:22:00 -0300

    refatorar função de guardar arquivo para melhorar legibilidade e modularidade

--- funcoes.py
+++ funcoes.py
@@ -1,48 +1,40 @@
-def armazenar_variavel(tabela_simbolos, var_nome, var_tipo, var_valor=None):
+def guardar_arquivo(arquivo_caminho, arquivo_conteudo):
+    with open(arquivo_caminho, 'r') as arquivo:
+        linhas = arquivo.readlines()
+        for linha in linhas:
+            linha = linha.strip()
+            if linha:
+                linha = ''.join(linha.split())
+                arquivo_conteudo.append(linha)
+    return arquivo_conteudo
+
+def declarar_variavel_tabela(tabela_simbolos, var_nome, var_tipo, var_valor=0):
     tabela_simbolos[var_nome] = {"tipo": var_tipo, "valor": var_valor}
 
 def atribuir_valor_variavel(tabela_simbolos, var_nome, var_valor):
-    if verificar_existencia_variavel(tabela_simbolos, var_nome):
-        tabela_simbolos[var_nome]["valor"] = var_valor
-    else:
-        print(f"Erro: Variável {var_nome} não definida neste escopo.")
+    tabela_simbolos[var_nome]["valor"] = var_valor
 
-def verificar_tipo_variavel(tabela_simbolos, var):
-    if verificar_existencia_variavel(tabela_simbolos, var):
-        var_tipo = tabela_simbolos[var]["tipo"]
-        return var_tipo
-    
-def verificar_valor_variavel(tabela_simbolos, var):
-    if verificar_existencia_variavel(tabela_simbolos, var):
-        var_valor = tabela_simbolos[var]["valor"]
-        return var_valor
-
-def atribuir_valor_entre_variaveis(tabela_simbolos, var_origem, var_destino):
-    if verificar_existencia_variavel(tabela_simbolos, var_origem) and verificar_existencia_variavel(tabela_simbolos, var_destino):
-        tipo_var_origem = verificar_tipo_variavel(tabela_simbolos, var_origem)
-        tipo_var_destino = verificar_tipo_variavel(tabela_simbolos, var_destino)
-        if tipo_var_origem == tipo_var_destino:
-            valor_var_origem = verificar_valor_variavel(tabela_simbolos, var_origem)
-            atribuir_valor_variavel(tabela_simbolos, var_destino, valor_var_origem)
-        else:
-            print(f"ERRO: Tipos incompatíveis - {tipo_var_origem} e {tipo_var_destino}")
-
-def verificar_existencia_variavel(tabela_simbolos, var):
-    if var in tabela_simbolos:
-        return True
-    else:
-        print(f"ERRO: Variável não declarada - {var}")
-        return False
-    
-def procurar_valor_variavel_em_escopos(tabela_simbolos, var_nome):
-    for escopo in reversed(tabela_simbolos):
+def procurar_variavel_em_pilha(pilha, var_nome):
+    for escopo in reversed(pilha):
         if var_nome in escopo:
-            return escopo[var_nome]['valor']
+            return True
+    return False
+
+def procurar_valor_variavel_em_pilha(pilha, var_nome):
+    for escopo in reversed(pilha):
+        if isinstance(escopo, dict) and var_nome in escopo:
+            return escopo[var_nome].get("valor", None)
     return None
-    
-def is_number(s):
+
+def procurar_tipo_variavel_em_pilha(pilha, var_nome):
+    for escopo in reversed(pilha):
+        if isinstance(escopo, dict) and var_nome in escopo:
+            return escopo[var_nome].get("tipo")
+    return None
+
+def e_numero(var):
     try:
-        float(s)
+        float(var)
         return True
     except ValueError:
         return False

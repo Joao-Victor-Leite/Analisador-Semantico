65ce660cb34634bae3d412eabf9bc2be560bac98
João Victor Leite <jvldsa10@hotmail.com>
2024-06-02 23:13:24 -0300

    Adição da documentacao da funcao menu e estilizacao no terminal

--- menu.py
+++ menu.py
@@ -1,16 +1,27 @@
 import os
 
 def menu(diretorio):
-    arquivos = os.listdir(diretorio)
+    """
+    Exibe um menu com os arquivos disponíveis em um diretório e permite ao usuário escolher um arquivo para abrir.
 
+    Args:
+        diretorio (str): O caminho do diretório onde estão os arquivos.
+
+    Retorno:
+        str: O caminho completo do arquivo escolhido pelo usuário.
+        None: Caso a escolha seja inválida.
+    """
+    arquivos = os.listdir(diretorio)
     arquivos_txt = sorted([arquivo for arquivo in arquivos if arquivo.endswith('.txt')])
 
     # Imprimir o menu
-    print("Arquivos disponíveis:")
+    print("\033[1;34;40mArquivos disponíveis:\033[0m")
+    print("\033[1;32;40m" + "=" * 30 + "\033[0m")
     for i, arquivo in enumerate(arquivos_txt, start=1):
-        print(f"{i}. {arquivo}")
+        print(f"\033[1;33;40m{i}. {arquivo}\033[0m")
+    print("\033[1;32;40m" + "=" * 30 + "\033[0m")
 
-    escolha = input("Digite o número do arquivo que deseja abrir: ")
+    escolha = input("\033[1;34;40mDigite o número do arquivo que deseja abrir:\033[0m")
 
     if escolha.isdigit() and 1 <= int(escolha) <= len(arquivos_txt):
         arquivo_escolhido = arquivos_txt[int(escolha)-1]
